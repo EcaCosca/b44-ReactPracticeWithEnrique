8ea152e27eed91866e7fc50c9f4032f6a9ff0e24
EcaCosca <enriquecoscarelli@gmail.com>
2023-10-24 09:56:22 +0200

    Going in deeper with useState

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,7 @@ function App() {
   // const count = 0
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+    <>        
       <div className="card">
       <h1>Count: {count}</h1>
         <button onClick={() => setCount((prevValueOfCount) => prevValueOfCount + 1)}>
@@ -27,15 +18,10 @@ function App() {
           Substract 1 to count
         </button>
         <button onClick={() => setCount(0)}>
-          reset count to 0
+          Reset count to 0
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
